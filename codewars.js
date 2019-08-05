7ed7c9b0799ca44092ce22a813e2c72676a4a854
George Howe <george@Georges-MacBook-Pro.local>
2019-08-04 19:17:43 -0700

    roman numerals kata

--- codewars.js
+++ codewars.js
@@ -46,8 +46,89 @@
 // accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
-function accum(s) {
-  return s.toUpperCase().split("").map((currentValue, index) => {return currentValue + currentValue.toLowerCase().repeat(index)}).join("-");
+//ANSWER:
+// function accum(s) {
+//   return s.toUpperCase().split("").map((currentValue, index) => {return currentValue + currentValue.toLowerCase().repeat(index)}).join("-");
+// }
+//
+// console.log(accum("RqaEzty"));
+
+
+
+
+
+
+// TITLE: Roman Numberals Encoder
+// LEVEL: 6 kyu
+// DESCRIPTION: Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+// Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+// EXAMPLE: solution(1000); // should return 'M'
+
+// ANSWER:
+function solution(number){
+  let romanNumeral = "";
+
+  while(number - 1000 >= 0){
+    number = number-1000;
+    romanNumeral += "M";
+  }
+  while(number - 500 >= 0){
+    if(number >= 900){
+      romanNumeral += "CM"
+      number = number-900;
+    } else{
+    number = number-500;
+    romanNumeral += "D";
+    }
+  }
+  while(number - 100 >= 0){
+    if(number >= 400){
+      romanNumeral += "CD"
+      number = number-400;
+    }else {
+      number = number-100;
+      romanNumeral += "C";
+    }
+  }
+  while(number - 50 >= 0){
+    if(number >= 90){
+      romanNumeral += "XC";
+      number = number-90;
+    }else {
+      number = number-50;
+      romanNumeral += "L";
+    }
+  }
+  while(number - 10 >= 0){
+    if(number >= 40){
+      romanNumeral += "XL"
+      number = number-40;
+    }else {
+      number = number-10;
+      romanNumeral += "X";
+    }
+
+  }
+  while(number - 5 >= 0){
+    if(number === 9){
+      romanNumeral += "IX"
+      number = number-9
+    } else{
+      number = number-5;
+      romanNumeral += "V";
+    }
+  }
+  while(number - 1 >= 0){
+    if(number === 4){
+      romanNumeral += "IV"
+      number = number-4;
+    }else {
+      number = number-1;
+      romanNumeral += "I";
+    }
+  }
+
+  return romanNumeral;
 }
 
-console.log(accum("RqaEzty"));
+console.log(solution(1990));
